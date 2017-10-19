fb5efef25cde04275cf2e04464949c8d9beebd9f
xyz-47 <ea_mahmoudi@esi.dz>
2017-10-19 09:27:18 -0400

    la fonction fractionner et son test

--- activity-1(1,2,3).py
+++ activity-1(1,2,3).py
@@ -21,7 +21,25 @@ class StringHelper:
                 sp = ''
             str += l +sp
         return  str
+    def fractionner(self, String, sp):
+        '''
+        :param String: la chaine de caractere
+        :param sp: le separateur
+        :return: une list des chaine deviser selon par le separateur
+        '''
+        list = []
+        i = 0
+        j = 0
+        for c in String:
+            if c in sp:
+                list.append(String[i:j])
+                j += 1
+                i = j
 
+            else:
+                j += 1
+        list.append(String[i:j])
+        return list
 
 
 
@@ -31,3 +49,7 @@ class TP_Test(unittest.TestCase):
     def test_join(self):
         list = ['rouge','vert','blanc']
         self.assertEqual(StringHelper.Joindre(self,list,'-'),'rouge-vert-blanc')
+    def test_fract(self):
+        list = ['rouge', 'vert', 'blanc']
+        str = 'rouge,vert,blanc'
+        self.assertEqual(StringHelper.fractionner(self, str, ','), list)
